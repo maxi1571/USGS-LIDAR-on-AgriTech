d3353cd23a389062d5d677ddb9af1a9740608764
Michael Tekle <michaeltekle1571@gmail.com>
2021-08-21 11:53:33 +0000

    Update edit_pipeline.py

--- script/edit_pipeline.py
+++ script/edit_pipeline.py
@@ -7,18 +7,12 @@ def open_json(path):
 
 def edit_pipeline(full_dataset_path, laz_path, tif_path, output_epsg, bound, polygon_input):
     fetch_json = open_json("./pipeline.json")
-    # fetch_json['pipeline'][0]['filename'] = full_dataset_path
-    # fetch_json['pipeline'][0]['bounds'] = bound
-    # fetch_json['pipeline'][3]['out_srs'] = f'EPSG:{output_epsg}'
-    # fetch_json['pipeline'][3]['filename'] = laz_path
-    # fetch_json['pipeline'][4]['filename'] = tif_path
-
     fetch_json['pipeline'][0]['filename'] = full_dataset_path
     fetch_json['pipeline'][0]['bounds'] = bound
     fetch_json['pipeline'][1]['polygon'] = polygon_input
     fetch_json['pipeline'][3]['out_srs'] = f'EPSG:{output_epsg}'
-    fetch_json['pipeline'][4]['filename'] = "../data/laz/" + laz_path + ".laz"
-    fetch_json['pipeline'][5]['filename'] = "../data/tif/" + tif_path + ".tif"
+    fetch_json['pipeline'][4]['filename'] = "./data/laz/"+laz_path
+    fetch_json['pipeline'][5]['filename'] = "./data/tif/"+tif_path
 
 
     return fetch_json

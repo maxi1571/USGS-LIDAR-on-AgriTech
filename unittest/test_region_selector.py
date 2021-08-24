81031796af5a7ad3fdc886fba9955e3c3fac7524
root <michaeltekle1571@gmail.com>
2021-08-24 19:48:54 +0300

    Update test file

--- unittest/test_region_selector.py
+++ unittest/test_region_selector.py
@@ -1,5 +1,15 @@
+import unittest
 from script.region_selector import dataset_path
 from script.region_selector import SelectName
 
-print(SelectName("IA_FullState"))
-print(dataset_path("IA_FullState"))
+class Test_region_selector(unittest.TestCase):
+
+    def test_SelectName(self):
+        self.assertEqual(print(SelectName("IA_FullState")), "IA_FullState")
+
+    def test_dataset_path(self):
+        self.assertEqual(print(dataset_path("IA_FullState"), "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/IA_FullState/ept.json"))
+
+
+if(__name__ == '__main__'):
+    unittest.main()
